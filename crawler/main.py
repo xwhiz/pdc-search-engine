2906bff15a4a346700a77b31158e0ef8ccda88f5
Hamza <muhammadhamzam1486@gmail.com>
2025-04-27 17:00:18 +0500

    refactor: update main function to use multiple drivers and threading for keyword scraping

--- crawler/main.py
+++ crawler/main.py
@@ -5,18 +5,16 @@ from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 import os
+import threading as th
 
-from src.get_search_results import get_search_results
+from src.scrape_keyword import scrape_keyword
 from src.constants import DATA_PATH
-from src.save_search_results import save_search_results
-from src.extract_content import extract_content
-from src.save_extracted_data import save_extracted_data
 
 
 def get_driver():
     options = webdriver.ChromeOptions()
     options.add_argument("--disable-dev-shm-usage")
-    options.page_load_strategy = "eager"
+    # options.page_load_strategy = "eager"
     # options.add_argument("headless")
     return webdriver.Chrome(
         service=Service(ChromeDriverManager().install()), options=options
@@ -25,30 +23,28 @@ def get_driver():
 
 def main():
     keywords = open(f"{DATA_PATH}/keywords.txt")
-    driver = get_driver()
+    drivers = [get_driver() for _ in range(2)]
 
-    keyword = next(keywords).strip()
-    save_search_results(driver, keyword)
+    threads = []
 
-    try:
-        search_results = get_search_results(keyword)
-    except FileNotFoundError:
-        save_search_results(driver, keyword)
-        search_results = get_search_results(keyword)
+    current_driver = 0
 
-    # if data is already extracted, skip it
-    if os.path.exists(f"{DATA_PATH}/{'-'.join(keyword.split())}/extracted_data.json"):
-        return
+    for keyword in list(keywords)[:10]:
+        t = th.Thread(target=scrape_keyword, args=(drivers[current_driver], keyword))
+        threads.append(t)
+        t.start()
+        current_driver = (current_driver + 1) % len(drivers)
 
-    extracted_data: Dict[str, Dict] = dict()
-    for url in map(lambda x: x["link"], search_results):
-        domain = urlparse(url).netloc
-        content = extract_content(driver, url)
-        extracted_data[domain] = content
+        if len(threads) == len(drivers):
+            for t in threads:
+                t.join()
+            threads = []
 
-    save_extracted_data(extracted_data, keyword)
+    # for t in threads:
+    #     t.join()
 
-    driver.quit()
+    for driver in drivers:
+        driver.quit()
 
 
 if __name__ == "__main__":
