c45dd8de20450d9e3eb21f1666937451ed57fa4b
Hamza <muhammadhamzam1486@gmail.com>
2025-04-27 16:13:59 +0500

    feat: add save_search_results function to handle search result storage

---
+++ crawler/src/save_search_results.py
@@ -0,0 +1,19 @@
+import os
+import json
+
+from .constants import DATA_PATH
+from .scrape_duckduckgo_results import scrape_duckduckgo_results
+
+
+def save_search_results(driver, keyword: str):
+    path = f"{DATA_PATH}/{'-'.join(keyword.split())}"
+    if os.path.exists(path):
+        return
+
+    os.makedirs(path)
+    print(f"Created directory: {path}")
+    search_results = scrape_duckduckgo_results(driver, keyword)
+
+    with open(f"{path}/search_results.json", "w") as f:
+        json.dump(search_results, f)
+        print(f"Saved search results to {path}/search_results.json")
