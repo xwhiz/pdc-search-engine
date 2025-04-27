802a1b44c3641371b46508a889d5723712c57154
Hamza <muhammadhamzam1486@gmail.com>
2025-04-27 16:44:43 +0500

    refactor: create directory for extracted data before saving JSON file

--- crawler/src/save_extracted_data.py
+++ crawler/src/save_extracted_data.py
@@ -1,10 +1,14 @@
+import os
 import json
 
 from .constants import DATA_PATH
 
 
 def save_extracted_data(data: dict, keyword: str):
-    path = f"{DATA_PATH}/{'-'.join(keyword.split())}/extracted_data.json"
-    with open(path, "w") as f:
+    path = f"{DATA_PATH}/{'-'.join(keyword.split())}"
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    with open(f"{path}/extracted_data.json", "w") as f:
         json.dump(data, f)
         print(f"Saved extracted data to {path}")
