04b3043fa43aa67b12c78bd359173ca75845ef0e
Hamza <muhammadhamzam1486@gmail.com>
2025-04-27 16:13:54 +0500

    feat: implement search result scraping and saving functionality

--- crawler/src/get_search_results.py
+++ crawler/src/get_search_results.py
@@ -1,21 +1,13 @@
-from typing import List, Dict
 import os
 import json
 
-from .scrape_duckduckgo_results import scrape_duckduckgo_results
+from .constants import DATA_PATH
 
 
-def get_search_results(driver, keyword: str) -> List[Dict[str, str]]:
-    path = f"../data/{'-'.join(keyword.split())}"
+def get_search_results(keyword: str) -> list:
+    path = f"{DATA_PATH}/{'-'.join(keyword.split())}/search_results.json"
     if not os.path.exists(path):
-        os.makedirs(path)
-        print(f"Created directory: {path}")
-        search_results = scrape_duckduckgo_results(driver, keyword)
+        raise FileNotFoundError(f"File not found: {path}")
 
-        with open(f"{path}/search_results.json", "w") as f:
-            json.dump(search_results, f)
-            print(f"Saved search results to {path}/search_results.json")
-    else:
-        with open(f"{path}/search_results.json", "r") as f:
-            search_results = json.load(f)
-            print(f"Loaded search results from {path}/search_results.json")
+    with open(path, "r") as f:
+        return json.load(f)
